f8a6b4d5ff373a82dee0d00a893775d2fcc56d86
Ganesh Burande <ganeshburande2027@gmail.com>
2024-05-08 15:11:28 +0530

    updatemodelbox

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
-    
 
+    // Get form data
     var email = document.getElementById("exampleInputEmail1").value;
     var password = document.getElementById("exampleInputPassword1").value;
     var isChecked = document.getElementById("exampleCheck1").checked;
 
-   
-    var displayDiv = document.getElementById("displayData");
-    displayDiv.innerHTML = "<h2>Form Data</h2>" +
+    // Set form data to display in modal
+    var modalBody = document.querySelector("#myModal .modal-body");
+    modalBody.innerHTML = "<h2>Form Data</h2>" +
                             "<p>Email: " + email + "</p>" +
                             "<p>Password: " + password + "</p>" +
                             "<p>Checkbox Checked: " + isChecked + "</p>";
 
-    
+    // Display the modal
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'), { backdrop: 'static', keyboard: false }); // Initialize modal
+    myModal.show(); // Show modal
+
+    // Log form data to console
     console.log("Email:", email);
     console.log("Password:", password);
     console.log("Checkbox Checked:", isChecked);
